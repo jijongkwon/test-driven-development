0d96ecc4f974fe9a2c723d69bf54d48df79a4a73
jijongkwon <help3451@naver.com>
2023-04-07 21:37:22 +0900

    refactor : 클래스명 변경

--- src/test/java/study/chap1/MoneyTest.java
+++ src/test/java/study/chap1/MoneyTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DollarTest {
+class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
