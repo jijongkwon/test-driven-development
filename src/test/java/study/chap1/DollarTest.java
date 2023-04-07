488c6fefc71c0b28678708df9215c0698e26aadf
jijongkwon <help3451@naver.com>
2023-04-07 20:36:48 +0900

    refactor : 필드 비교가 아닌 객체를 통한 비교로 변경

--- src/test/java/study/chap1/DollarTest.java
+++ src/test/java/study/chap1/DollarTest.java
@@ -8,8 +8,7 @@ class DollarTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
     }
 
     @Test
