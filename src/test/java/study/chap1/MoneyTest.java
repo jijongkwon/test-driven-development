c7b6f1a998f8111e569ce99023c41752201d0313
jijongkwon <help3451@naver.com>
2023-04-10 16:22:09 +0900

    test : 더하기 함수 테스트

--- src/test/java/study/chap1/MoneyTest.java
+++ src/test/java/study/chap1/MoneyTest.java
@@ -13,19 +13,16 @@ class MoneyTest {
     }
 
     @Test
-    public void testEquality(){
-        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        assertTrue(Money.franc(5).equals(Money.franc(5)));
-        assertFalse(Money.franc(5).equals(Money.franc(6)));
-        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    public void testSimpleAddition(){
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
     }
 
     @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
+    public void testEquality(){
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));;
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
     }
 
     @Test
