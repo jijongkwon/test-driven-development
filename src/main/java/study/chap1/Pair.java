ee78eee4acffc54af049872677bfe391782a35b0
jijongkwon <help3451@naver.com>
2023-05-10 16:44:08 +0900

    feat : Pair 클래스 생성 - 두 개의 통화와 환율을 매핑시키는 해시 테이블을 사용할 예정

---
+++ src/main/java/study/chap1/Pair.java
@@ -0,0 +1,11 @@
+package study.chap1;
+
+public class Pair {
+    private String from;
+    private String to;
+
+    Pair(String from, String to){
+        this.from = from;
+        this.to = to;
+    }
+}
