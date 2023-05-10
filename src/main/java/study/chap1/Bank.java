e4b9ff2325476017113400257fa16648f1b15c7c
jijongkwon <help3451@naver.com>
2023-05-10 17:01:36 +0900

    feat : 환율 비율을 저장하는 함수 추가

--- src/main/java/study/chap1/Bank.java
+++ src/main/java/study/chap1/Bank.java
@@ -1,11 +1,21 @@
 package study.chap1;
 
+import java.util.Hashtable;
+
 public class Bank {
+    private Hashtable rates = new Hashtable();
+
     Money reduce(Expression source, String to){
         return source.reduce(this, to);
     }
 
     int rate(String from, String to){
-        return (from.equals("CHF") && to.equals("USD")) ? 2 : 1;
+        if(from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
+    }
+
+    void addRate(String from, String to, int rate){
+        rates.put(new Pair(from, to), new Integer(rate));
     }
 }
